1caca76ec8ec6e10cf21ca336bd94608e35296e0
Ahmed Elashry <91360603+AhmedElachry@users.noreply.github.com>
2025-03-19 20:11:33 +0200

    [feat] updated footer

--- src/containers/layout/Footer/index.tsx
+++ src/containers/layout/Footer/index.tsx
@@ -2,8 +2,21 @@ import { Footer as AntdFooter } from "antd/es/layout/layout";
 
 function Footer() {
   return (
-    <AntdFooter className=" p-0 bg-transparent h-[45px] flex items-center justify-center whitespace-nowrap overflow-hidden text-ellipsis">
-       Secop ©{new Date().getFullYear()}<span className="inline-flex gap-1 ms-1"> By <a rel="noopener noreferrer" target="_blank" href="https://www.appssquare.sa"> {" "}Appssquare</a></span> 
+    <AntdFooter
+      className=" p-0 bg-transparent h-[45px] flex items-center justify-center whitespace-nowrap overflow-hidden text-ellipsis"
+      style={{ direction: "ltr" }}
+    >
+      Secop © {new Date().getFullYear()}
+      <span className="inline-flex gap-1 ms-1">
+        By
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://satr.net.sa/"
+        >
+          Satr
+        </a>
+      </span>
     </AntdFooter>
   );
 }
